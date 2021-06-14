b81254ea04e3d9888f06e0f81800e8d3c7bf9842
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-06-14 19:48:23 +0200

    sfdzvgbujkihlx

--- public/intra/script.js
+++ public/intra/script.js
@@ -33,6 +33,13 @@ const displayInput = async () => {
 
     page.innerHTML = /*html*/`
         <h1>Velkommen til Intra</h1>
+
+        <div id="productList">
+            <table id="productListTable">
+
+            </table>
+        </div>
+
         <h2>↓ /users/logout</h2>
         <div class="form" id="logout">
             <button id="logout-submit">Logout</button>
@@ -96,14 +103,12 @@ const registerHandler = async () => {
     const username = document.getElementById('register-username').value;
     const password = document.getElementById('register-password').value;
     if(!username || !password) return alert('Malformed inputs');
-
     const url = '/api/users/register';
     const headers = new Headers()
     headers.append('Content-Type', 'application/json');
     const body = JSON.stringify({username, password});
     const method = 'POST';
     const result = await (await fetch(url, {headers, body, method})).json();
-
     if(result.success) alert('User created: ' + result.user);
     else alert('An error occurred:\n' + result.response);
 }
@@ -115,14 +120,12 @@ const productCreateHandler = async () => {
     const price = document.getElementById('product-create-price').value;
     const images = document.getElementById('product-create-images').value.split('\n');
     if(!(title && description && price && images)) return alert('Malformed input');
-
     const url = '/api/products/create';
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     const body = JSON.stringify({token, title, description, price, images});
     const method = 'POST';
     const result = await (await fetch(url, {headers, body, method})).json();
-
     if(result.success) alert('Product created: ' + JSON.stringify(result.product, null, 4));
     else alert('An error occurred:\n' + result.response);
 }
@@ -131,14 +134,12 @@ const productDeleteHandler = async () => {
     const token = sessionStorage.getItem('token');
     const id = document.getElementById('product-delete-id').value;
     if(!id) return alert('Malformed input');
-
     const url = '/api/products/delete';
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     const body = JSON.stringify({token, id});
     const method = 'POST';
     const result = await (await fetch(url, {headers, body, method})).json();
-
     if(result.success) alert('Product deleted: ' + JSON.stringify(result.product, null, 4));
     else alert('An error occurred:\n' + result.response);
 }
@@ -147,14 +148,12 @@ const carouselSetHandler = async () => {
     const token = sessionStorage.getItem('token');
     const products = document.getElementById('carousel-set-products').value.split('\n');
     if(!products) return alert('Malformed input');
-
     const url = '/api/carousel/set';
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     const body = JSON.stringify({token, products});
     const method = 'POST';
     const result = await (await fetch(url, {headers, body, method})).json();
-
     if(result.success) alert('Carousel set: ' + JSON.stringify(result.product, null, 4));
     else alert('An error occurred:\n' + result.response);
 }
@@ -167,11 +166,40 @@ const setEventHandlers = () => {
     document.getElementById('carousel-set-submit').addEventListener('click', () => carouselSetHandler());
 }
 
+const makeProductList = async () => {
+    document.getElementById('productListTable').innerHTML = '';
+    const products = await getProducts();
+    for(let i in products) {
+        document.getElementById('productListTable').innerHTML += /*html*/ `
+            <tr>
+                <td><p>${products[i].title}</p></td>
+                <td><p>${products[i].price}</p></td>
+                <td><button id="productListDeleteButtonWithId${i}">Delete</button></td>
+            </tr>
+        `;
+        document.getElementById(`productListDeleteButtonWithId${i}`).addEventListener('click', async () => {
+            const token = sessionStorage.getItem('token');
+            const id = products[i].id;
+            if(!id) return alert('Malformed input');
+            const url = '/api/products/delete';
+            const headers = new Headers();
+            headers.append('Content-Type', 'application/json');
+            const body = JSON.stringify({token, id});
+            const method = 'POST';
+            const result = await (await fetch(url, {headers, body, method})).json();
+            if(result.success) alert('Product deleted: ' + JSON.stringify(result.product, null, 4));
+            else alert('An error occurred:\n' + result.response);
+            makeProductList();
+        });
+    }
+}
+
 const main = async () => {
     if(!sessionStorage.getItem('token')) location.pathname = '/intra/login';
     await checkToken();
     await displayInput();
     setEventHandlers();
+    makeProductList();
 }
 
 main();
