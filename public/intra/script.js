d3fec9b840f2f1b9845658411e1c26603b8e7164
piet <42157469+camper0008@users.noreply.github.com>
2021-06-14 17:47:16 +0200

    intra page html

--- public/intra/script.js
+++ public/intra/script.js
@@ -18,11 +18,48 @@ const checkToken = async () => {
 const displayInput = async () => {
     page.innerHTML = /*html*/`
         <h1>Velkommen til Intra</h1>
-        <h2>/users/logout</h2>
-        <h2>/users/register</h2>
-        <h2>/products/create</h2>
-        <h2>/products/delete</h2>
-        <h2>/carousel/set</h2>
+        <h2>↓ /users/logout</h2>
+        <div class="form" id="logout">
+            <button id="logout-submit">Logout</button>
+        </div>
+        <h2>↓ /users/register</h2>
+        <div class="form" id="register">
+            <label for="register-username">Brugernavn</label>
+            <input id="register-username">
+
+            <label for="register-password">Adgangskode</label>
+            <input id="register-password" type="password">
+            <button id="register-submit">Register</button>
+        </div>
+        <h2>↓ /products/create</h2>
+        <!--title, description, price, images-->
+        <div class="form" id="product-create">
+            <label for="product-create-title">Produkt Titel</label>
+            <input id="product-create-title">
+
+            <label for="product-create-description">Produkt Description</label>
+            <textarea id="product-create-description"></textarea>
+
+            <label for="product-create-price">Produkt Pris</label>
+            <input id="product-create-price" type="number">
+
+            <label for="product-create-links">Produkt Billeder</label>
+            <textarea id="product-create-links" placeholder='[ image.com/image0, image.com/image1 ]'></textarea>
+
+            <button id="product-create-submit">Create Product</button>
+        </div>
+        <h2>↓ /products/delete</h2>
+        <div class="form" id="product-delete">
+        
+            <label for="product-delete-id">Produkt Id</label>
+            <input id="product-delete-id">
+
+            <button id="product-delete-submit">Delete Product</button>
+        </div>
+        <h2>→ /carousel/set</h2>
+        <div class="form">
+            <p>jeg ved ik hvordan den fungerer lol</p>
+        </div>
     `
 }
 
