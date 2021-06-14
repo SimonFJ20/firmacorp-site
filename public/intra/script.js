caf42378957689306bea8e9b3a59c84f4847deaa
piet <42157469+camper0008@users.noreply.github.com>
2021-06-14 19:21:02 +0200

    dropdown menu

--- public/intra/script.js
+++ public/intra/script.js
@@ -15,7 +15,22 @@ const checkToken = async () => {
     }
 }
 
-const displayInput = () => {
+const getProducts = async () => {
+    const url = '/api/products/search/';
+    const result = await (await fetch(url)).json();
+
+    if(!result.success) alert('An error occurred:\n' + result.response);
+
+    return result.products;
+}
+
+const displayInput = async () => {
+
+    let dropdownString = '';
+    (await getProducts()).forEach(product => {
+        dropdownString += /*html*/`<option value=${product.id}>${product.title}</option>`
+    });
+
     page.innerHTML = /*html*/`
         <h1>Velkommen til Intra</h1>
         <h2>↓ /users/logout</h2>
@@ -49,6 +64,10 @@ const displayInput = () => {
         </div>
         <h2>↓ /products/delete</h2>
         <div class="form" id="product-delete">
+
+
+            <label for="product-delete-dropdown">Vælg id</label>
+            <select id="product-delete-dropdown">${dropdownString}</select>
         
             <label for="product-delete-id">Produkt Id</label>
             <input id="product-delete-id">
@@ -57,7 +76,10 @@ const displayInput = () => {
         </div>
         <h2>→ /carousel/set</h2>
         <div class="form">
-            <label for="carousel-set-products">Produkts</label>
+            <label for="carousel-set-dropdown">Indsæt id fra liste</label>
+            <select id="carousel-set-dropdown">${dropdownString}</select>
+
+            <label for="carousel-set-products">Produkter</label>
             <textarea id="carousel-set-products"></textarea>
 
             <button id="carousel-set-submit">Set Carousel Products</button>
@@ -74,12 +96,14 @@ const registerHandler = async () => {
     const username = document.getElementById('register-username').value;
     const password = document.getElementById('register-password').value;
     if(!username || !password) return alert('Malformed inputs');
+
     const url = '/api/users/register';
     const headers = new Headers()
     headers.append('Content-Type', 'application/json');
     const body = JSON.stringify({username, password});
     const method = 'POST';
     const result = await (await fetch(url, {headers, body, method})).json();
+
     if(result.success) alert('User created: ' + result.user);
     else alert('An error occurred:\n' + result.response);
 }
@@ -91,12 +115,14 @@ const productCreateHandler = async () => {
     const price = document.getElementById('product-create-price').value;
     const images = document.getElementById('product-create-images').value.split('\n');
     if(!(title && description && price && images)) return alert('Malformed input');
+
     const url = '/api/products/create';
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     const body = JSON.stringify({token, title, description, price, images});
     const method = 'POST';
     const result = await (await fetch(url, {headers, body, method})).json();
+
     if(result.success) alert('Product created: ' + JSON.stringify(result.product, null, 4));
     else alert('An error occurred:\n' + result.response);
 }
@@ -105,12 +131,14 @@ const productDeleteHandler = async () => {
     const token = sessionStorage.getItem('token');
     const id = document.getElementById('product-delete-id').value;
     if(!id) return alert('Malformed input');
+
     const url = '/api/products/delete';
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     const body = JSON.stringify({token, id});
     const method = 'POST';
     const result = await (await fetch(url, {headers, body, method})).json();
+
     if(result.success) alert('Product deleted: ' + JSON.stringify(result.product, null, 4));
     else alert('An error occurred:\n' + result.response);
 }
@@ -119,12 +147,14 @@ const carouselSetHandler = async () => {
     const token = sessionStorage.getItem('token');
     const products = document.getElementById('carousel-set-products').value.split('\n');
     if(!products) return alert('Malformed input');
+
     const url = '/api/carousel/set';
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     const body = JSON.stringify({token, products});
     const method = 'POST';
     const result = await (await fetch(url, {headers, body, method})).json();
+
     if(result.success) alert('Carousel set: ' + JSON.stringify(result.product, null, 4));
     else alert('An error occurred:\n' + result.response);
 }
@@ -140,7 +170,7 @@ const setEventHandlers = () => {
 const main = async () => {
     if(!sessionStorage.getItem('token')) location.pathname = '/intra/login';
     await checkToken();
-    displayInput();
+    await displayInput();
     setEventHandlers();
 }
 
