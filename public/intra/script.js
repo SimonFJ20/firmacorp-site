62b7c8e4c1d06ce46074d9e363c74af98f41005f
piet <42157469+camper0008@users.noreply.github.com>
2021-06-14 20:20:39 +0200

    fixed table formatting and added toggle visibility for api

--- public/intra/script.js
+++ public/intra/script.js
@@ -26,25 +26,20 @@ const getProducts = async () => {
 
 const displayInput = async () => {
 
-    let dropdownString = '';
-    (await getProducts()).forEach(product => {
-        dropdownString += /*html*/`<option value=${product.id}>${product.title}</option>`
-    });
-
     page.innerHTML = /*html*/`
         <h1>Velkommen til Intra</h1>
 
-        <div id="productList">
-            <table id="productListTable">
+        <div id="product-list">
+            <table id="product-list-table">
 
             </table>
         </div>
 
-        <h2>↓ /users/logout</h2>
+        <h2 ><button form="logout"> - </button> /users/logout</h2>
         <div class="form" id="logout">
             <button id="logout-submit">Logout</button>
         </div>
-        <h2>↓ /users/register</h2>
+        <h2><button form="register"> - </button> /users/register</h2>
         <div class="form" id="register">
             <label for="register-username">Brugernavn</label>
             <input id="register-username">
@@ -53,7 +48,7 @@ const displayInput = async () => {
             <input id="register-password" type="password">
             <button id="register-submit">Register</button>
         </div>
-        <h2>↓ /products/create</h2>
+        <h2><button form="product-create"> - </button> /products/create</h2>
         <div class="form" id="product-create">
             <label for="product-create-title">Produkt Titel</label>
             <input id="product-create-title">
@@ -69,22 +64,16 @@ const displayInput = async () => {
 
             <button id="product-create-submit">Create Product</button>
         </div>
-        <h2>↓ /products/delete</h2>
+        <h2><button form="product-delete"> - </button> /products/delete</h2>
         <div class="form" id="product-delete">
-
-
-            <label for="product-delete-dropdown">Vælg id</label>
-            <select id="product-delete-dropdown">${dropdownString}</select>
         
             <label for="product-delete-id">Produkt Id</label>
             <input id="product-delete-id">
 
             <button id="product-delete-submit">Delete Product</button>
         </div>
-        <h2>→ /carousel/set</h2>
-        <div class="form">
-            <label for="carousel-set-dropdown">Indsæt id fra liste</label>
-            <select id="carousel-set-dropdown">${dropdownString}</select>
+        <h2><button form="carousel-set"> - </button> /carousel/set</h2>
+        <div class="form" id="carousel-set">
 
             <label for="carousel-set-products">Produkter</label>
             <textarea id="carousel-set-products"></textarea>
@@ -92,6 +81,18 @@ const displayInput = async () => {
             <button id="carousel-set-submit">Set Carousel Products</button>
         </div>
     `
+
+    document.querySelectorAll('button[form]').forEach( (element) => {
+        element.addEventListener('click', () => {
+            if (element.innerText === '-') {
+                element.innerText = '+'
+                document.getElementById(element.attributes.form.value).hidden = true;
+            } else {
+                document.getElementById(element.attributes.form.value).hidden = false;
+                element.innerText = '-'
+            }
+        })
+    })
 }
 
 const logoutHandler = () => {
@@ -167,17 +168,25 @@ const setEventHandlers = () => {
 }
 
 const makeProductList = async () => {
-    document.getElementById('productListTable').innerHTML = '';
+    document.getElementById('product-list-table').innerHTML = /*html*/`
+    <tr>
+        <th>Titel</th>
+        <th>Pris</th>
+        <th>Id</th>
+        <th>Slet</th>
+    </tr>
+    `;
     const products = await getProducts();
     for(let i in products) {
-        document.getElementById('productListTable').innerHTML += /*html*/ `
+        document.getElementById('product-list-table').innerHTML += /*html*/ `
             <tr>
                 <td><p>${products[i].title}</p></td>
                 <td><p>${products[i].price}</p></td>
-                <td><button id="productListDeleteButtonWithId${i}">Delete</button></td>
+                <td><p>${products[i].id}</p></td>
+                <td><button id="product-list-delete-${i}">Delete</button></td>
             </tr>
         `;
-        document.getElementById(`productListDeleteButtonWithId${i}`).addEventListener('click', async () => {
+        document.getElementById(`product-list-delete-${i}`).addEventListener('click', async () => {
             const token = sessionStorage.getItem('token');
             const id = products[i].id;
             if(!id) return alert('Malformed input');
